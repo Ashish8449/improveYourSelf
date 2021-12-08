287e48003d88fc2bc9cb99a14a3a6f65476e78c8
ashish8449 <ashishgururani8449@gmail.com>
2021-12-08 18:31:56 +0530

    check_mates

--- Check_Mate.cpp
+++ Check_Mate.cpp
@@ -10,49 +10,55 @@
 Comments will be included in practice problems if it helps ^^
 */
 #include <bits/stdc++.h>
-typedef long             long ll;
-typedef long             double ld;
-#define rep(i,n,k)       for(ll i=0;i<n;i+=k)
-#define rrep(i,n,k)      for(ll i=n;i>=0;i-=k)
-#define rep1(i,n,k)      for(ll i=1;i<n;i+=k)
-#define vi               vector<int>
-#define vl               vector<ll>
-#define vs               vector<string>
-#define vvi              vector<vi>
-#define vvl              vector<vl>
-#define vvs              vector<vs>
-#define mem(a,b)         memset(a,b,sizeof(a))
-#define fast             ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define pb               emplace_back
-#define mp               make_pair
-#define fi               first
-#define se               second
-#define endl          "\n"
-#define tc(t)            long long t;cin>>t;while(t--)
-#define all(x)           (x).begin(), (x).end()
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n, k) for (ll i = 0; i < n; i += k)
+#define rrep(i, n, k) for (ll i = n; i >= 0; i -= k)
+#define rep1(i, n, k) for (ll i = 1; i < n; i += k)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define vvs vector<vs>
+#define mem(a, b) memset(a, b, sizeof(a))
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define endl "\n"
+#define tc(t)    \
+    long long t; \
+    cin >> t;    \
+    while (t--)
+#define all(x) (x).begin(), (x).end()
 
-#define dbg(x)           cout<<#x<<" = "<<x<<endl
-#define inf                 1e6+5
-#define mod              ll(1e9+7)
+#define dbg(x) cout << #x << " = " << x << endl
+#define inf 1e6 + 5
+#define mod ll(1e9 + 7)
 using namespace std;
 
-template<typename T>
+template <typename T>
 void print_array(const T &arr, char c = ' ')
 {
-for (auto x : arr)
-{
-cout << x << c;
-}
-cout << endl;
+    for (auto x : arr)
+    {
+        cout << x << c;
+    }
+    cout << endl;
 }
 
-template<typename T>
-void input_array(vector< T> &arr)
+template <typename T>
+void input_array(vector<T> &arr)
 {
-for (ll i = 0; i < arr.size(); i++)
-{
-cin>>arr[i];
-}
+    for (ll i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
 }
 
 vvl adj;
@@ -60,17 +66,18 @@ vl vis;
 int main()
 {
 #ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("error.txt", "w", stderr);
-freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
 #endif
-fast;
-ll a, b,c,d,e,f,m,n,p,q;
-string s,r;
+    fast;
+    ll a, b, c, d, e, f, m, n, p, q;
+    string s, r;
 
-tc(t)
-{
-     
-}
-return 0;
+    tc(t)
+    {
+
+        //  think about this problem:🏆
+    }
+    return 0;
 }
