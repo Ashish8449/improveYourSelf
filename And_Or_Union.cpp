dd48de6b4ad38e15f715100a3dbc229428eef5a9
ashish8449 <ashishgururani8449@gmail.com>
2022-01-05 22:01:06 +0530

    5jan

---
+++ And_Or_Union.cpp
@@ -0,0 +1,116 @@
+/*
+*
+*************************************************************
+* AUTHOR : Ashish Gururani                                  *
+* Language: C++14                                           *
+* Purpose: -                                                *
+* IDE used: Visual Studio Code.                             *
+*************************************************************
+*
+Comments will be included in practice problems if it helps ^^
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n, k) for (ll i = 0; i < n; i += k)
+#define rrep(i, n, k) for (ll i = n; i >= 0; i -= k)
+#define rep1(i, n, k) for (ll i = 1; i < n; i += k)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define vvs vector<vs>
+#define mem(a, b) memset(a, b, sizeof(a))
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define endl "\n"
+#define tc(t)    \
+    long long t; \
+    cin >> t;    \
+    while (t--)
+#define all(x) (x).begin(), (x).end()
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define inf 1e6 + 5
+#define mod ll(1e9 + 7)
+using namespace std;
+
+template <typename T>
+void print_array(const T &arr, char c = ' ')
+{
+    for (auto x : arr)
+    {
+        cout << x << c;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void input_array(vector<T> &arr)
+{
+    for (ll i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+vvl adj;
+vl vis;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast;
+    ll a, b, c, d, e, f, m, n, p, q;
+    string s, r;
+
+    tc(t)
+    {
+
+        cin >> n;
+        vector<bitset<32>> v(n);
+        for (ll i = 0; i < n; i++)
+        {
+            cin >> a;
+            v[i] = bitset<32>(a);
+        }
+        // print_array(v);
+        bitset<32> ans;
+        // dbg(ans);
+        for (ll j = 0; j < 32; j++)
+        {
+            ll count = 0;
+            for (ll i = 0; i < n; i++)
+            {
+                // dbg(v[i]);
+                // dbg(i);
+                if (v[i][j])
+                {
+                    count++;
+                }
+            }
+            // dbg(count);
+            if (count > 1)
+            {
+                ans[j] = 1;
+            }
+        }
+        ll value = 0;
+        for (ll i = 0; i < 32; i++)
+        {
+            value += long(pow(2, i)) * ans[i];
+        }
+        cout << value << endl;
+    }
+    return 0;
+}
