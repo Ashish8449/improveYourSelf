ea73978424b45132ba8fc8c5d2e4d0bea0d16b1f
ashish8449 <ashishgururani8449@gmail.com>
2022-02-08 23:35:00 +0530

    8feb

--- Bitwise_Blend.cpp
+++ Bitwise_Blend.cpp
@@ -10,183 +10,129 @@
 Comments will be included in practice problems if it helps ^^
 */
 #include <bits/stdc++.h>
-typedef long long ll;
-typedef long double ld;
-#define rep(i, n, k) for (ll i = 0; i < n; i += k)
-#define rrep(i, n, k) for (ll i = n; i >= 0; i -= k)
-#define rep1(i, n, k) for (ll i = 1; i < n; i += k)
-#define vi vector<int>
-#define vl vector<ll>
-#define vs vector<string>
-#define vvi vector<vi>
-#define vvl vector<vl>
-#define vvs vector<vs>
-#define mem(a, b) memset(a, b, sizeof(a))
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define pb emplace_back
-#define mp make_pair
-#define fi first
-#define se second
-#define endl "\n"
-#define tc(t)    \
-    long long t; \
-    cin >> t;    \
-    while (t--)
-#define all(x) (x).begin(), (x).end()
+typedef long             long ll;
+typedef long             double ld;
+#define rep(i,n,k)       for(ll i=0;i<n;i+=k)
+#define rrep(i,n,k)      for(ll i=n;i>=0;i-=k)
+#define rep1(i,n,k)      for(ll i=1;i<n;i+=k)
+#define vi               vector<int>
+#define vl               vector<ll>
+#define vs               vector<string>
+#define vvi              vector<vi>
+#define vvl              vector<vl>
+#define vvs              vector<vs>
+#define mem(a,b)         memset(a,b,sizeof(a))
+#define fast             ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb               emplace_back
+#define mp               make_pair
+#define fi               first
+#define se               second
+#define endl          "\n"
+#define tc(t)            long long t;cin>>t;while(t--)
+#define all(x)           (x).begin(), (x).end()
 
-#define dbg(x) cout << #x << " = " << x << endl
-#define inf 1e6 + 5
-#define mod ll(1e9 + 7)
+#define dbg(x)           cout<<#x<<" = "<<x<<endl
+#define inf                 1e6+5
+#define mod              ll(1e9+7)
 using namespace std;
 
-template <typename T>
+template<typename T>
 void print_array(const T &arr, char c = ' ')
 {
-    for (auto x : arr)
-    {
-        cout << x << c;
-    }
-    cout << endl;
+for (auto x : arr)
+{
+cout << x << c;
+}
+cout << endl;
 }
 
-template <typename T>
-void input_array(vector<T> &arr)
+template<typename T>
+void input_array(vector< T> &arr)
 {
-    for (ll i = 0; i < arr.size(); i++)
-    {
-        cin >> arr[i];
-    }
+for (ll i = 0; i < arr.size(); i++)
+{
+cin>>arr[i];
+}
 }
 
 vvl adj;
 vl vis;
-bool parity(ll a, ll b) { return a % 2 == b % 2; }
-vector<vector<pair<int, int>>> ans_main;
-void fun0(vl v, int indx)
-{
-   
-    vector<pair<int, int>> ans;
-    if (!indx % 2)
-    {
-        if (v[1] % 2)
-        {
-            indx = 1;
-        }
-        else
-        {
-            v[1] ^= v[indx];
-            ans.pb(2, indx + 1);
-            indx = 1;
-        }
-    }
-   
-    for (int i = 0; i < v.size(); i++)
-    {
-        if ((v[i] % 2 != i % 2))
-        {
-            //  if(indx==i)
-           
-            ans.pb(i + 1, indx + 1);
-            v[i] ^= v[indx];
-        }
-    }
-    // print_array(v);
-   
-// for (int i = 0; i < ans.size(); i++)    
-//     {
-//         cout<<ans[i].first<<" "<<ans[i].second<<endl;
-//     }
-    ans_main.pb(ans);
-}
-void fun1(vl v, int indx)
-{
-
-//  dbg(indx);
-    vector<pair<int, int>> ans;
-    if ( indx % 2)
-    {
-        if (v[0] % 2)
-        {
-            indx = 0;
-        }
-        else
-        {
-            v[0] ^= v[indx];
-            ans.pb(1, indx + 1);
-            indx = 0;
-        }
-    }
-//  print_array(v);
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (v[i] % 2 == i % 2)
-        {
-             
-
-            ans.pb(i + 1, indx + 1);
-            v[i] ^= v[indx];
-        }
-    }
-    // for (int i = 0; i < ans.size(); i++)    
-    // {
-    //     cout<<ans[i].first<<" "<<ans[i].second<<endl;
-    // }
-    
-    
-
-    ans_main.pb(ans);
-}
-
 int main()
 {
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("error.txt", "w", stderr);
-    freopen("output.txt", "w", stdout);
+freopen("input.txt", "r", stdin);
+freopen("error.txt", "w", stderr);
+freopen("output.txt", "w", stdout);
 #endif
-    fast;
-    ll a, b, c, d, e, f, m, n, p, q;
-    string s, r;
+fast;
+ll a, b,c,d,e,f,m,n,p,q;
+string s,r;
 
-    tc(t)
-    {
-        //  how many odd at its correct position:
-        // ans_main.clear();
-        ll count_odd = 0, count_even = 0;
-        cin >> n;
-        vl v(n);
-        ll odd = -1;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> v[i];
-            if (v[i] & 1)
-            {
-                odd = i;
-            }
-        }
-        if (odd > -1)
-        {
-            
-            fun1(v, odd);
-            fun0(v, odd);
-        }
-        if (odd == -1)
-        {
-            cout << odd << endl;
-            continue;
-        }
-        int j = 0;
-        if (ans_main.size() == 2 && ans_main[1].size() < ans_main[0].size())
-            j++;
+tc(t)
+{
+    cin>>n;
+     vl v(n);
+     input_array(v);
+     ll count01=0, count10=0;
+     vector<pair<int,int>> odd;
+     for (int i = 0; i < n; i++)
+     {
+        count01+=((i&1)==(v[i]&1));
+        count10+=((i&1)!=(v[i]&1));
+        if(v[i]&1)odd.pb(mp(v[i], i));
+      
+        
+     }
+    //  dbg(count10);
+    //  dbg(count01);
+  if(!odd.size())cout<<-1<<endl;
+  else if(count10>count01){
+      cout<<n-count10<<endl;
+      for (int i = 0; i < n; i++)
+      {
+          
+          if(((i&1)!=(v[i]&1)))continue;
+         else if(odd.back().second==i){
+            odd.pop_back();
+             cout<<i+1<<" "<<odd.back().second+1<<endl;
+             if(!(v[i]&1))
+              odd.pb(mp(v[i]^odd.back().first, i));
+              
+          }else{
+               cout<<i+1<<" "<<odd.back().second+1<<endl;
+                if(!(v[i]&1))
+              odd.pb(mp(v[i]^odd.back().first, i));
+          }
+      }
+      
 
-        cout << ans_main[j].size() << endl;
-        for (int i = 0; i < ans_main[j].size(); i++)
-        {
-            cout << ans_main[j][i].first << " " << ans_main[j][i].second << endl;
-        }
-    }
-    return 0;
+
+  }else{
+        cout<<n-count01<<endl;
+      for (int i = 0; i < n; i++)
+      {
+          if((i&1)==(v[i]&1))continue;
+         else if(odd.back().second==i){
+            odd.pop_back();
+              cout<<i+1<<" "<<odd.back().second+1<<endl;
+               if(!(v[i]&1))
+              odd.pb(mp(v[i]^odd.back().first, i));
+              
+          }else{
+              cout<<i+1<<" "<<odd.back().second+1<<endl;
+               if(!(v[i]&1))
+              odd.pb(mp(v[i]^odd.back().first, i));
+          }
+      }
+
+
+  }
+//   print_array(v);
+     
+     
+     
+
+     
+}
+return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,2 @@
 
-}
+      cout<<count10<<endl;
