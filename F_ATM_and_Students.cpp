2bc5fa0f772ba255e1b3bda543c9a81ab6d60afe
ashish8449 <ashishgururani8449@gmail.com>
2022-02-28 20:01:10 +0530

    28feb

---
+++ F_ATM_and_Students.cpp
@@ -0,0 +1,112 @@
+/*
+*
+*************************************************************
+* AUTHOR : Ashish Gururani                                  *
+* Language: C++14                                           *
+* Purpose: -                                                *
+* IDE used: Visual Studio Code.                             *
+*************************************************************
+*
+Comments will be included in practice problems if it helps ^^
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n, k) for (ll i = 0; i < n; i += k)
+#define rrep(i, n, k) for (ll i = n; i >= 0; i -= k)
+#define rep1(i, n, k) for (ll i = 1; i < n; i += k)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define vvs vector<vs>
+#define mem(a, b) memset(a, b, sizeof(a))
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define endl "\n"
+#define tc(t)    \
+    long long t; \
+    cin >> t;    \
+    while (t--)
+#define all(x) (x).begin(), (x).end()
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define inf 1e6 + 5
+#define mod ll(1e9 + 7)
+using namespace std;
+
+template <typename T>
+void print_array(const T &arr, char c = ' ')
+{
+    for (auto x : arr)
+    {
+        cout << x << c;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void input_array(vector<T> &arr)
+{
+    for (ll i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+vvl adj;
+vl vis;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast;
+    ll a, b, c, d, e, f, m, n, p, q;
+    string s, r;
+
+    tc(t)
+    {
+        ll money;
+        cin >> n >> money ;
+        vl v(n);
+        input_array(v);
+        ll dif = 0;
+        ll start, end;
+        ll current_money = 0;
+        start = 0;
+        int i = 0;
+        ll ans1, ans2;
+        while (i < n&&start <n)
+        {
+           if(current_money+money+v[i]>=0){
+               current_money+=v[i];
+               i++;
+           }else if(i==start){
+               current_money=v[i];
+               i++;
+           }
+           else{
+               current_money-=v[start];
+               if(dif<i-start){
+                   dif =i-start;
+               }
+
+               start++;
+           }
+          dbg(current_money+money);
+        }
+        dbg(dif);
+        dbg(start);
+    }
+    return 0;
+}
