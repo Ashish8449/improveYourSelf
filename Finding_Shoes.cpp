6f0cc3cfe2e5fb24343f85b5e81b1a956ec71f4b
ashish8449 <ashishgururani8449@gmail.com>
2022-03-29 21:57:05 +0530

    29march

--- Finding_Shoes.cpp
+++ Finding_Shoes.cpp
@@ -1,78 +1,53 @@
-/*
-*
-*************************************************************
-* AUTHOR : Ashish Gururani                                  *
-* Language: C++14                                           *
-* Purpose: -                                                *
-* IDE used: Visual Studio Code.                             *
-*************************************************************
-*
-Comments will be included in practice problems if it helps ^^
-*/
+
 #include <bits/stdc++.h>
-typedef long             long ll;
-typedef long             double ld;
-#define rep(i,n,k)       for(ll i=0;i<n;i+=k)
-#define rrep(i,n,k)      for(ll i=n;i>=0;i-=k)
-#define rep1(i,n,k)      for(ll i=1;i<n;i+=k)
-#define vi               vector<int>
-#define vl               vector<ll>
-#define vs               vector<string>
-#define vvi              vector<vi>
-#define vvl              vector<vl>
-#define vvs              vector<vs>
-#define mem(a,b)         memset(a,b,sizeof(a))
-#define fast             ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define pb               emplace_back
-#define mp               make_pair
-#define fi               first
-#define se               second
-#define endl          "\n"
-#define tc(t)            long long t;cin>>t;while(t--)
-#define all(x)           (x).begin(), (x).end()
-
-#define dbg(x)           cout<<#x<<" = "<<x<<endl
-#define inf                 1e6+5
-#define mod              ll(1e9+7)
 using namespace std;
+#define ff first
+#define ss second
+#define int long long
+#define ull unsigned long long
+#define endl "\n"
+#define pb emplace_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define vs vector<string>
+#define mii map<int, int>
+#define pqb priority_queue<int>
+#define lb(x, n) lower_bound(all(x), n)
+#define ub(x, n) upper_bound(all(x), n)
+#define all(x) x.begin(), x.end()
+#define maxi(x) x[max_element(all(x)) - x.begin()]
+#define mini(x) x[min_element(all(x)) - x.begin()]
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define tc(t) \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
 
-template<typename T>
-void print_array(const T &arr, char c = ' ')
+int32_t main()
 {
-for (auto x : arr)
-{
-cout << x << c;
-}
-cout << endl;
-}
-
-template<typename T>
-void input_array(vector< T> &arr)
-{
-for (ll i = 0; i < arr.size(); i++)
-{
-cin>>arr[i];
-}
-}
-
-vvl adj;
-vl vis;
-int main()
-{
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("error.txt", "w", stderr);
-freopen("output.txt", "w", stdout);
-#endif
-fast;
-ll a, b,c,d,e,f,m,n,p,q;
-string s,r;
-
-tc(t)
-{
-    cin>>n>>m;
-    cout<<n+ n- min(m, n)<<endl;
-     
-}
-return 0;
+    fast;
+    tc(t)
+    {
+        int N, M;
+        cin >> N >> M;
+        if (M > N)
+        {
+            cout << N << endl;
+        }
+        else
+        {
+            cout << 2 * N - M << endl;
+        }
+    }
+    return 0;
 }
