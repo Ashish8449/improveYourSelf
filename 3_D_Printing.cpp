b752b24ed5ebbe46adb88afcb5523e0d015b17a5
ashish8449 <ashishgururani8449@gmail.com>
2022-04-02 22:14:27 +0530

    2april

---
+++ 3_D_Printing.cpp
@@ -0,0 +1,112 @@
+/*
+*
+*************************************************************
+* AUTHOR : Ashish Gururani                                  *
+* Language: C++14                                           *
+* Purpose: -                                                *
+* IDE used: Visual Studio Code.                             *
+*************************************************************
+*
+Comments will be included in practice problems if it helps ^^
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n, k) for (ll i = 0; i < n; i += k)
+#define rrep(i, n, k) for (ll i = n; i >= 0; i -= k)
+#define rep1(i, n, k) for (ll i = 1; i < n; i += k)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define vvs vector<vs>
+#define mem(a, b) memset(a, b, sizeof(a))
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define endl "\n"
+#define tc(t)    \
+    long long t; \
+    cin >> t;    \
+    while (t--)
+#define all(x) (x).begin(), (x).end()
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define inf 1e6 + 5
+#define mod ll(1e9 + 7)
+using namespace std;
+
+template <typename T>
+void print_array(const T &arr, char c = ' ')
+{
+    for (auto x : arr)
+    {
+        cout << x << c;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void input_array(vector<T> &arr)
+{
+    for (ll i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+vvl adj;
+vl vis;
+int main()
+{
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("error.txt", "w", stderr);
+//     freopen("output.txt", "w", stdout);
+// #endif
+    fast;
+    ll a, b, c, d, e, f, m, n, p, q;
+    string s, r;
+
+    int testcase = 0;
+    cin >> testcase;
+    for (int t = 1; t <= testcase; t++)
+
+    {
+
+        vl ans(4, INT64_MAX);
+        vl v(4);
+        d = 1e6;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                cin >> a;
+                ans[j] = min(ans[j], a);
+            }
+        }
+        cout << "Case #" << t << ": ";
+        ll sum = accumulate(all(ans), 0LL);
+        if(sum>=1e6){
+            for (int i = 0; i < 4; i++)
+            {
+                b = min(ans[i], d);
+                d-=b;
+                cout<<b<<" ";
+            }
+            cout<<endl;
+
+        }
+        else
+        {
+            cout << "IMPOSSIBLE" << endl;
+        }
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,2 @@
- // for (int i = 0; i < adj.size(); i++)
-        // {
-        //     print_array(adj[i]);
-        // }
+
+    for (int i = 0; i < 2*m +1; i++)
