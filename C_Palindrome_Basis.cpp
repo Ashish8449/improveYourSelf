1f95e376c9ba4f53319b28d40ebefd59a05b9d70
ashish8449 <ashishgururani8449@gmail.com>
2022-04-25 22:29:10 +0530

    25april

---
+++ C_Palindrome_Basis.cpp
@@ -0,0 +1,120 @@
+/*
+*
+*************************************************************
+* AUTHOR : Ashish Gururani                                  *
+* Language: C++14                                           *
+* Purpose: -                                                *
+* IDE used: Visual Studio Code.                             *
+*************************************************************
+*
+Comments will be included in practice problems if it helps ^^
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n, k) for (ll i = 0; i < n; i += k)
+#define rrep(i, n, k) for (ll i = n; i >= 0; i -= k)
+#define rep1(i, n, k) for (ll i = 1; i < n; i += k)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define vvs vector<vs>
+#define mem(a, b) memset(a, b, sizeof(a))
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define endl "\n"
+#define tc(t)    \
+    long long t; \
+    cin >> t;    \
+    while (t--)
+#define all(x) (x).begin(), (x).end()
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define inf 1e6 + 5
+#define mod ll(1e9 + 7)
+using namespace std;
+
+template <typename T>
+void print_array(const T &arr, char c = ' ')
+{
+    for (auto x : arr)
+    {
+        cout << x << c;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void input_array(vector<T> &arr)
+{
+    for (ll i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+vvl adj;
+vl vis;
+bool isPali(int num)
+{
+    int rev = 0, rem, temp;
+    temp = num;
+    while (temp > 0)
+    {
+        rem = temp % 10;
+        rev = (rev * 10) + rem;
+        temp = temp / 10;
+    }
+    return (rev == num);
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast;
+    ll a, b, c, d, e, f, m, n, p, q;
+    string s, r;
+    vi numbers;
+    for (int i = 1; i < 4e4 + 1; i++)
+    {
+        if (isPali(i))
+        {
+            numbers.pb(i);
+        }
+    }
+    // print_array(numbers, '\n');
+    n = numbers.size();
+    ll target = 4e4;
+    vector<vector<int>> dp(n + 1, vector<int>(4e4 + 1, 0));
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= target; j++)
+        {
+            dp[i][j] = dp[i - 1][j];
+            int left = j - numbers[i - 1];
+            if (left >= 0)
+            {
+                (dp[i][j] += dp[i][left]) %= mod;
+            }
+        }
+    }
+
+    tc(t)
+    {
+        cin >> n;
+        cout << dp[numbers.size()][n] << endl;
+    }
+    return 0;
+}
