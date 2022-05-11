ea10909ddc7eeed94515fa236189fed346b727be
ashish8449 <ashishgururani8449@gmail.com>
2022-05-11 23:37:12 +0530

    11may

---
+++ Good_Pairs.cpp
@@ -0,0 +1,105 @@
+/*
+*
+*************************************************************
+* AUTHOR : Ashish Gururani                                  *
+* Language: C++14                                           *
+* Purpose: -                                                *
+* IDE used: Visual Studio Code.                             *
+*************************************************************
+*
+Comments will be included in practice problems if it helps ^^
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n, k) for (ll i = 0; i < n; i += k)
+#define rrep(i, n, k) for (ll i = n; i >= 0; i -= k)
+#define rep1(i, n, k) for (ll i = 1; i < n; i += k)
+#define vi vector<int>
+#define vl vector<ll>
+#define vs vector<string>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define vvs vector<vs>
+#define mem(a, b) memset(a, b, sizeof(a))
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define endl "\n"
+#define tc(t)    \
+    long long t; \
+    cin >> t;    \
+    while (t--)
+#define all(x) (x).begin(), (x).end()
+
+#define dbg(x) cout << #x << " = " << x << endl
+#define inf 1e6 + 5
+#define mod ll(1e9 + 7)
+using namespace std;
+
+template <typename T>
+void print_array(const T &arr, char c = ' ')
+{
+    for (auto x : arr)
+    {
+        cout << x << c;
+    }
+    cout << endl;
+}
+
+template <typename T>
+void input_array(vector<T> &arr)
+{
+    for (ll i = 0; i < arr.size(); i++)
+    {
+        cin >> arr[i];
+    }
+}
+
+vvl adj;
+vl vis;
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    fast;
+    ll a, b, c, d, e, f, m, n, p, q;
+    string s, r;
+
+    tc(t)
+    {
+        cin >> n;
+        vl A(n), B(n);
+        input_array(A);
+        input_array(B);
+        map<pair<ll, ll>, ll> map;
+        for (int i = 0; i < n; i++)
+        {
+            map[mp(A[i], B[i])]++;
+        }
+        ll ans = 0, add = 0;
+        for (auto &&i : map)
+        {
+            auto p2 = mp(i.first.second, i.first.first);
+            if (i.first.second == i.first.first)
+            {
+                add += (map[p2] * (map[p2] - 1)) / 2;
+            }
+            else
+            {
+                ans += (map[p2] * i.second);
+                i.second = 0;
+            }
+        }
+        cout << ans + add << endl;
+    }
+    return 0;
+}
